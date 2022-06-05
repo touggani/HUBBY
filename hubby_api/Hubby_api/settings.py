25f22ce838bfdb49affd5de3c3851ae04cbed3af
touggani <youssef.touggani@hotmail.com>
2022-06-05 14:46:57 +0200

    deploy settings

--- hubby_api/Hubby_api/settings.py
+++ hubby_api/Hubby_api/settings.py
@@ -30,7 +30,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "django-insecure-8@-_2mkox^nmk0f3alw9zv_xw%-qd*9cfj#qp#+c9sa4-#+a87"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['127.0.0.1', '.herokuapp.com']
 
