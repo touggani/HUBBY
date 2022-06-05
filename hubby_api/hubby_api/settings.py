50d21c41c4568a7f40060685976deafe3ce57411
touggani <youssef.touggani@hotmail.com>
2022-06-05 13:28:46 +0200

    modify procfile

--- hubby_api/hubby_api/settings.py
+++ hubby_api/hubby_api/settings.py
@@ -26,8 +26,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = config('SECRET_KEY')
-
+#SECRET_KEY = config('SECRET_KEY')
+SECRET_KEY = "django-insecure-8@-_2mkox^nmk0f3alw9zv_xw%-qd*9cfj#qp#+c9sa4-#+a87"
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
