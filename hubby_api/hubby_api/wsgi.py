538f69b7b9bc531367f22b8818b9c56ada6f84d3
touggani <youssef.touggani@hotmail.com>
2022-06-05 13:19:20 +0200

    modify procfile

--- hubby_api/hubby_api/wsgi.py
+++ hubby_api/hubby_api/wsgi.py
@@ -8,9 +8,12 @@ https://docs.djangoproject.com/en/4.0/howto/deployment/wsgi/
 """
 
 import os
-
+import sys
 from django.core.wsgi import get_wsgi_application
 
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+sys.path.append(BASE_DIR)
+os.environ['DJANGO_SETTINGS_MODULE'] = 'Hubby_api.settings'
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Hubby_api.settings')
 
 application = get_wsgi_application()
